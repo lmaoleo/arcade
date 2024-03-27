cd283b21f6e23c4956b123c207b24fa573292948
Matthias SOUAL <matthias.soual@epitech.eu>
2024-03-27 18:08:27 +0100

    ADD: charmap with emojis

--- src/graphic/Ncurses.cpp
+++ src/graphic/Ncurses.cpp
@@ -6,13 +6,27 @@
 */
 
 #include <ncurses.h>
+#include <wchar.h>
+#include <locale.h>
 #include "Ncurses.hpp"
+
+static const std::map<std::string, wchar_t> charmap = {
+    {"wall", L'🧱'},
+    {"snake_head_down", L'🐍'},
+    {"snake_head_up", L'🐍'},
+    {"snake_head_left", L'🐍'},
+    {"snake_head_right", L'🐍'},
+    {"snake_body", L'🟩'},
+    {"food", L'🍔'},
+    {"empty", L' '},
+};
 
 graphic::Ncurses::Ncurses()
 {
     initscr();
     noecho();
     curs_set(0);
+    setlocale(LC_ALL, "");
     keypad(stdscr, TRUE);
     _keys.bindKey("UP", KEY_UP);
     _keys.bindKey("DOWN", KEY_DOWN);
@@ -23,20 +37,26 @@ graphic::Ncurses::Ncurses()
 
 void graphic::Ncurses::updateKeybinds()
 {
+    std::string action;
     int key = getch();
     if (key == KEY_UP)
-        _keys.keyPressed("UP", true);
+        action = "UP";
+        _keys.keyPressed(action, true);
     if (key == KEY_DOWN)
-        _keys.keyPressed("DOWN", true);
+        action = "DOWN";
+        _keys.keyPressed(action, true);
     if (key == KEY_LEFT)
-        _keys.keyPressed("LEFT", true);
+        action = "LEFT";
+        _keys.keyPressed(action, true);
     if (key == KEY_RIGHT)
-        _keys.keyPressed("RIGHT", true);
+        action = "RIGHT";
+        _keys.keyPressed(action, true);
     if (key == 27)
-        _keys.keyPressed("ESC", true);
+        action = "ESC";
+        _keys.keyPressed(action, true);
 }
 
 std::queue<state::Event> graphic::Ncurses::draw()
 {
-    
+
 }
